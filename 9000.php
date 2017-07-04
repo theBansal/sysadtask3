798e857c48ab61d4bc1cdc47e06f2ff00f293cfb
theBansal <byashbansal46@gmail.com>
2017-07-05 03:08:28 +0530

    Add files via upload

---
+++ 9000.php
@@ -0,0 +1,20 @@
+<?php
+ $username=$_POST['username'];
+ $password=$_POST['password'];
+ $fullname=$_POST['FullName'];
+ $email=$_POST['email'];
+ 
+ $cryptpass=SHA1($password);
+$dbc = mysqli_connect('127.0.0.1','root','Y123y123','db1'); 
+if ($stmt = $mysqli->prepare("INSERT INTO formdata (username,password,fullname,email) VALUES ($username,$cryptpass,$fullname,$email)")) {
+ 
+    // Bind the variables to the parameter as strings. 
+    $stmt->bind_param("ssss", $username, $cryptpass,$fullname,$email);
+ 
+    // Execute the statement.
+    $stmt->execute();
+ 
+    // Close the prepared statement.
+    $stmt->close();
+}
+?>
---
+++ 9001.php
@@ -0,0 +1,26 @@
+<?php
+$username=$_POST['username'];
+$password=$_POST['password'];
+
+$dbc = mysqli_connect('127.0.0.1','root','Y123y123','db1'); 
+if ($stmt = $mysqli->prepare("SELECT username,fullname,email FROM formdata WHERE username=$username and password=SHA1($password)")) {
+ 
+    // Bind the variables to the parameter as strings. 
+    $stmt->bind_param("ssss", $username, $cryptpass,$fullname,$email);
+ 
+    // Execute the statement.
+    $stmt->execute();
+     
+    // Get the variables from the query.
+    $stmt->bind_result($un,$fn,$email);
+ 
+    // Fetch the data.
+    $stmt->fetch();
+ 
+    // Display the data.
+    printf(" Hello %s your username is  %s and your email is \n", $fn, $un, %email);
+	
+    // Close the prepared statement.
+    $stmt->close();
+}
+?>
